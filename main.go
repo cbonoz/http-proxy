3a7ada791c5d3da7da71ad21cc2e3e09d7b5ccc5
Chris Buonocore <chrisdistrict@gmail.com>
2022-10-18 22:54:19 -0400

    add up

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"*"},
-		AllowHeaders:     []string{"Origin"},
+		AllowHeaders:     []string{"Origin", "X-Requested-With", "Content-Type", "Accept"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
@@ -23,6 +23,9 @@ func main() {
 	r.Use(logger.SetLogger())
 
 	r.POST("/proxy", controllers.ProxyRequest)
+	r.GET("up", func(c *gin.Context) {
+		c.JSON(200, gin.H{"hello": "world"})
+	})
 	// listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 	r.Run(":8080")
 }
